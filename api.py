1ef88272075287e54d8f6414fa435cc89bf7dd34
adityavasisht <aditya.vasisht0@gmail.com>
2026-01-22 11:00:54 +0530

    Updated API and added frontend URL.

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://YOUR_FRONTEND.vercel.app",
+        "https://launchpad-next-tau.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
